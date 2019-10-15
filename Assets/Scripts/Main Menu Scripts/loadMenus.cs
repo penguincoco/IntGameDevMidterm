3538787d47334298213db948bfd744943ccd1502
Samantha Chuang <samantha.s.chuang@gmail.com>
2019-10-15 21:27:38 +0900

    Added a bit more to the main menu to make it more functional

---
+++ Assets/Scripts/Main Menu Scripts/loadMenus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//usage: put this on a button
+//intent: turn off Main Menu Canvas to activate How To Play Canvas and vice Versa
+
+public class loadMenus : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Canvas howTo; 
+
+    public void loadHowTo() {
+        mainMenu.gameObject.SetActive(false);
+        howTo.gameObject.SetActive(true);
+    }
+
+    public void loadMainMenu() {
+        mainMenu.gameObject.SetActive(true);
+        howTo.gameObject.SetActive(false);
+    }
+}
